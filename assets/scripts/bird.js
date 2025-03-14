87016e72283594c89ff9ad5abb8c19d0e1e11a25
Anastasia Balagurova <73748770+balagurova@users.noreply.github.com>
2025-03-15 00:20:33 +0200

    Update bird shape

--- assets/scripts/bird.js
+++ assets/scripts/bird.js
@@ -1,59 +1,65 @@
 let BirdShapes = {};
 
-BirdShapes.bird = function (p, x, y, size, color, wingsOption = null) {
+BirdShapes.bird = function (
+  p,
+  x,
+  y,
+  size,
+  color,
+  wingsOption = null,
+  mirrorX = null,
+  rotationAngle = null
+) {
+  p.push();
+  p.translate(x, y);
+
+  mirrorX = mirrorX !== null ? mirrorX : p.random() > 0.5;
+
+  rotationAngle = rotationAngle !== null ? rotationAngle : p.random(-30, 30);
+
+  let scaleX = mirrorX ? -1 : 1;
+  p.scale(scaleX, 1);
+  p.rotate(p.radians(rotationAngle));
+
   p.fill(color);
+
   p.beginShape();
-
-  p.vertex(x - size / 2, y);
+  p.vertex(-size / 2, size / 5);
   p.bezierVertex(
-    x - size / 4,
-    y - size / 8,
-    x + size / 4,
-    y - size / 8,
-    x + size / 2,
-    y
+    size / 3,
+    -size / 6,
+    size / 0.6,
+    -size / 6,
+    -p.random(size / 5, size / 2),
+    p.random(size / 4, size / 3)
   );
-
-  p.bezierVertex(
-    x + size / 4,
-    y + size / 8,
-    x - size / 4,
-    y + size / 8,
-    x - size / 2,
-    y
-  );
-
+  p.vertex(-size / 2, size / 5);
   p.endShape(p.CLOSE);
 
-  wingsOption = wingsOption !== null ? wingsOption : p.floor(p.random(1, 4));
+  wingsOption = wingsOption !== null ? wingsOption : p.floor(p.random(1, 3));
 
-  if (wingsOption === 1 || wingsOption === 3) {
+  if (wingsOption === 1) {
+    // First wing style
     p.beginShape();
-    p.vertex(x, y);
-    p.bezierVertex(
-      x - size / 3,
-      y - size / 4,
-      x + size / 3,
-      y - size / 2,
-      x + size / 2,
-      y - size / 3
-    );
-    p.vertex(x, y);
+    p.vertex(-size / 2, -size / 2);
+    p.quadraticVertex(size / 2, -size / 2, size / 1.5, size / 1.5);
     p.endShape(p.CLOSE);
   }
 
-  if (wingsOption === 2 || wingsOption === 3) {
+  if (wingsOption === 2) {
     p.beginShape();
-    p.vertex(x, y);
-    p.bezierVertex(
-      x - size / 3,
-      y + size / 4,
-      x + size / 3,
-      y + size / 2,
-      x + size / 2,
-      y + size / 3
-    );
-    p.vertex(x, y);
+    p.vertex(-size / 1.5, 0);
+    p.quadraticVertex(0, -size / 2, size / 1.5, 0);
     p.endShape(p.CLOSE);
+
+    p.push();
+    p.rotate(p.radians(40));
+    p.beginShape();
+    p.vertex(-size / 1.5, 0);
+    p.quadraticVertex(0, -size / 1.7, size / 2, -size / 2.5);
+    p.endShape(p.CLOSE);
+    p.pop();
   }
+
+  p.pop();
 };
